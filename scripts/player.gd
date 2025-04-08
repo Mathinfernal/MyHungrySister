7df1c03aa47f212ade44b0d64edbeb01c58b2b61
Mathinfernal <115974098+Mathinfernal@users.noreply.github.com>
2025-04-08 13:02:31 +0200

    Update player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,4 +7,14 @@ enum States { IDLE, MOVE }
 var currentState = States.IDLE
 
 func _physics_process(delta: float) -> void:
+	
+	handle_state_transitions()
+	perform_state_actions(delta)
 	move_and_slide()
+
+func handle_state_transitions():
+	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_up") or Input.is_action_pressed("ui_down"):
+		currentState = States.MOVE
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, movement_speed * 100)
+		currentState = States.IDLE
