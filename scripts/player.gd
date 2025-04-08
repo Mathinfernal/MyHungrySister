7182dab9387cf92360481479d75bd3efb00262bf
Mathinfernal <115974098+Mathinfernal@users.noreply.github.com>
2025-04-08 12:52:42 +0200

    Update player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,5 +3,8 @@ extends CharacterBody2D
 var movement_speed : float = 60.0
 var character_direction : Vector2
 
+enum States { IDLE, MOVE }
+var currentState = States.IDLE
+
 func _physics_process(delta: float) -> void:
 	move_and_slide()
